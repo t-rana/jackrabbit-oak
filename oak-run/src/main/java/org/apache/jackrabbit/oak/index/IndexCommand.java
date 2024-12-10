92e9020246a5099d22cd7929a67a03efb49615d3
Julian Reschke <reschke@apache.org>
2024-12-10 13:10:45 +0100

    OAK-11297: remove usage of Guava Joiner (#1889)
    * OAK-11297: remove usage of Guava Joiner - oak-upgrade
    
    * OAK-11297: remove usage of Guava Joiner - oak-store-document
    
    * OAK-11297: remove usage of Guava Joiner - oak-store-document
    
    * OAK-11297: remove usage of Guava Joiner - oak-store-document
    
    * OAK-11297: remove usage of Guava Joiner - oak-segment-tar
    
    * OAK-11297: remove usage of Guava Joiner - oak-security-spi
    
    * OAK-11297: remove usage of Guava Joiner - oak-run-elastic
    
    * OAK-11297: remove usage of Guava Joiner - oak-run-commons
    
    * OAK-11297: remove usage of Guava Joiner - oak-run
    
    * OAK-11297: remove usage of Guava Joiner - oak-run
    
    * OAK-11297: remove usage of Guava Joiner - oak-lucene
    
    * OAK-11297: remove usage of Guava Joiner - oak-jcr
    
    * OAK-11297: remove usage of Guava Joiner - oak-examples
    
    * OAK-11297: remove usage of Guava Joiner - oak-core
    
    * OAK-11297: remove usage of Guava Joiner - oak-commons
    
    * OAK-11297: remove usage of Guava Joiner - oak-blob-plugins
    
    * OAK-11297: remove usage of Guava Joiner - oak-benchmarks-lucene
    
    * OAK-11297: remove usage of Guava Joiner - oak-benchmarks
    
    * OAK-11297: remove usage of Guava Joiner - remove incorrect import
    
    * OAK-11297: remove usage of Guava Joiner - fix remaining issues in oak-run
    
    * OAK-11247: Migrated groovy test files to Java (#1851)
    
    * OAK-11247: Migrated groovy test files to Java
    
    * OAK-11247: Removed groovy file after rebase
    
    Removed gmavenplus plugin from pom
    
    * OAK-11291 Tree store: speed up merging, and concurrent indexing (#1885)
    
    * OAK-11298 Fix test IncrementalStoreTest (#1888)
    
    ---------
    
    Co-authored-by: Alejandro Moratinos <Amoratinos@users.noreply.github.com>
    Co-authored-by: Thomas Mueller <thomasm@apache.org>

--- oak-run/src/main/java/org/apache/jackrabbit/oak/index/IndexCommand.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/index/IndexCommand.java
@@ -18,7 +18,6 @@
  */
 package org.apache.jackrabbit.oak.index;
 
-import org.apache.jackrabbit.guava.common.base.Joiner;
 import org.apache.jackrabbit.guava.common.base.Stopwatch;
 import org.apache.jackrabbit.guava.common.collect.Sets;
 import org.apache.jackrabbit.guava.common.io.Closer;
@@ -412,7 +411,7 @@ public class IndexCommand implements Command {
     }
 
     private static void logCliArgs(String[] args) {
-        log.info("Command line arguments used for indexing [{}]", Joiner.on(' ').join(args));
+        log.info("Command line arguments used for indexing [{}]", String.join(" ", args));
         List<String> inputArgs = ManagementFactory.getRuntimeMXBean().getInputArguments();
         if (!inputArgs.isEmpty()) {
             log.info("System properties and vm options passed {}", inputArgs);
